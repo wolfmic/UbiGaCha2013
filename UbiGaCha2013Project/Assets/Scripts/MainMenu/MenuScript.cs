eaad20efc4cd02bba46c0dfe41bde144d3b4a256
Baptiste Careil <careil.baptiste@gmail.com>
2013-11-19 00:28:53 +0100

    Finalisation du menu : Ajout des sons de selection et validation, modifications des couleurs.

--- UbiGaCha2013Project/Assets/Scripts/MainMenu/MenuScript.cs
+++ UbiGaCha2013Project/Assets/Scripts/MainMenu/MenuScript.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 public class MenuScript : MonoBehaviour {
 
 	private GameObject _cursor;
+	private GameObject _menuSelectSoundEmitter;
+	private GameObject _menuValidateSoundEmitter;
 	private List<GameObject> _menuEntries;
 	private int _menuIndex;
 
@@ -13,6 +15,8 @@ public class MenuScript : MonoBehaviour {
 	void Start () {
 		_menuIndex = 0;
 		_cursor = GameObject.Find ("Cursor");
+		_menuSelectSoundEmitter = GameObject.Find("MenuSelectSoundEmitter");
+		_menuValidateSoundEmitter = GameObject.Find("MenuValidateSoundEmitter");
 		_menuEntries = new List<GameObject>(GameObject.FindGameObjectsWithTag("MenuEntry"));
 
 		_menuEntries.Sort((a, b) => (int)(b.transform.position.y >= a.transform.position.y ? (b.transform.position.y == a.transform.position.y ? 0 : 1) : -1));
@@ -30,10 +34,12 @@ public class MenuScript : MonoBehaviour {
 		// process input
 		if (Input.GetKeyDown ("down")) {
 			updateCursor = true;
+			_menuEntries[_menuIndex].renderer.material.color = Color.white;
 			_menuIndex = System.Math.Min(_menuEntries.Count - 1, _menuIndex + 1);
 		}
 		if (Input.GetKeyDown ("up")) {
 			updateCursor = true;
+			_menuEntries[_menuIndex].renderer.material.color = Color.white;
 			_menuIndex = System.Math.Max(0, _menuIndex - 1);
 		}
 		if (Input.GetKeyDown("space")) {
@@ -45,16 +51,19 @@ public class MenuScript : MonoBehaviour {
 			if (obj == null)
 				obj = _menuEntries[_menuIndex].GetComponent<StartScript>();
 			if (obj != null) {
+				_menuValidateSoundEmitter.audio.Play();
 				obj.SendMessage("Activate");
 			}
 		}
 		// move cursor if needed
 		if (updateCursor) {
+			_menuSelectSoundEmitter.audio.Play();
 			this.UpdateCursor();
 		}
 	}
 
 	void UpdateCursor() {
+		_menuEntries[_menuIndex].renderer.material.color = Color.yellow;
 		_cursor.transform.position = _menuEntries[_menuIndex].transform.position;
 	}
 }
