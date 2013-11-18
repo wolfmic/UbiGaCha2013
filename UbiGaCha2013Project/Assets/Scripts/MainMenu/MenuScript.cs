1479264d5d7a982ec257b109be8ec44c324da0b3
Baptiste Careil <careil.baptiste@gmail.com>
2013-11-18 16:30:53 +0100

    Ajout d'une scene MainMenu

---
+++ UbiGaCha2013Project/Assets/Scripts/MainMenu/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MenuScript : MonoBehaviour {
+
+	private GameObject _cursor;
+	private List<GameObject> _menuEntries;
+	private int _menuIndex;
+
+	// Use this for initialization
+	void Start () {
+		_menuIndex = 0;
+		_cursor = GameObject.Find ("Cursor");
+		_menuEntries = new List<GameObject>(GameObject.FindGameObjectsWithTag("MenuEntry"));
+
+		_menuEntries.Sort((a, b) => (int)(b.transform.position.y >= a.transform.position.y ? (b.transform.position.y == a.transform.position.y ? 0 : 1) : -1));
+		this.UpdateCursor ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool updateCursor = false;
+
+		// process input
+		if (Input.GetKeyDown ("down")) {
+			updateCursor = true;
+			_menuIndex = System.Math.Min(_menuEntries.Count - 1, _menuIndex + 1);
+		}
+		if (Input.GetKeyDown ("up")) {
+			updateCursor = true;
+			_menuIndex = System.Math.Max(0, _menuIndex - 1);
+		}
+		// move cursor if needed
+		if (updateCursor) {
+			this.UpdateCursor();
+		}
+	}
+
+	void UpdateCursor()
+	{
+		_cursor.transform.position = _menuEntries[_menuIndex].transform.position;
+	}
+}
