1b5e4af34b50f35a388d48d69e70810697d62ef8
Aymeric Girault <aymeric.girault@epitech.eu>
2013-11-19 09:01:56 +0100

    Player Controller Added
    Player controller on rails

---
+++ UbiGaCha2013Project/Assets/Scripts/Stairs/StairsToScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StairsToScript : MonoBehaviour {
+
+    public Vector3 dir;
+
+
+
+    void Start() {
+
+
+
+    }
+
+    void Update() {
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "Player") {
+            Debug.Log("SET DIRECTION");
+            other.GetComponent<Movements>().setDirection(dir);
+        }
+
+        
+    
+    }
+
+
+
+}
